be4715baa097c2a70bf01f03fa6e89f565e5030c
maria <mar1aaa.popova@gmail.com>
2025-10-29 15:18:02 +0700

    Fix flake8 style errors

--- service.py
+++ service.py
@@ -1,5 +1,6 @@
 def add_income(balance, amount):
     return balance + amount
 
+
 def add_expense(balance, amount):
     return balance - amount
--- tests/test_service.py
+++ tests/test_service.py
@@ -1,9 +1,10 @@
-import sys, os
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-
+import pytest
 from service import add_income, add_expense
+
+
 def test_add_income():
     assert add_income(100, 50) == 150
 
+
 def test_add_expense():
-    assert add_expense(200, 50) == 150
+    assert add_expense(100, 30) == 70
